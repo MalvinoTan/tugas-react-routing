ba1ea3f23806e6da04fee8b80e507b3419b5db0e
Malvino Austin Tanura <malvinoaustintanura@gmail.com>
2022-03-19 23:08:13 +0700

    Mengupdate Not found atribut alt

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -6,7 +6,7 @@ import styles from "./style.module.css";
 const NotFound = () => {
     return (
         <>
-            <img className={styles.not_found_img} src={img} alt="Not Found Image" />
+            <img className={styles.not_found_img} src={img} alt="Not Found" />
         </>
     );
 }
